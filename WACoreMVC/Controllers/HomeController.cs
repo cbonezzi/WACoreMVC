d1dbea2a210d3a9e764f84fd91880d550391b1b7
Cesar Bonezzi <cesar.bonezzi@emergingtek.net>
2019-04-15 19:54:33 -0500

    Add project files.

---
+++ WACoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WACoreMVC.Models;
+
+namespace WACoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult User()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44345/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync("api/values/users").Result;
+            var model = response.Content.ReadAsAsync<IList<UserModel>>();
+
+            var userList = new List<UserModel>();
+            userList.AddRange(model.Result);
+
+            return View(userList);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WACoreMVC/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WACoreMVC.Models
+{
+    public class UserModel
+    {
+        public string UserId { get; set; }
+        public string Expire { get; set; }
+        public string Username { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Phone { get; set; }
+    }
+}
