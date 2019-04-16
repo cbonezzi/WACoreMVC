eb7ff48b40e1deeab242ddc01ff0a77772c394d5
cesar.bonezzi <cesar.bonezzi@gmail.com>
2019-04-16 13:48:31 -0500

    test methods for edit and save

--- WACoreMVC/Controllers/HomeController.cs
+++ WACoreMVC/Controllers/HomeController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using WACoreMVC.Models;
 
 namespace WACoreMVC.Controllers
@@ -35,6 +37,35 @@ namespace WACoreMVC.Controllers
             userList.AddRange(model.Result);
 
             return View(userList);
+        }
+
+        public IActionResult EditUser(string userId)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44345/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync("api/values/" + userId).Result;
+            var model = response.Content.ReadAsAsync<UserModel>();
+
+            return View(model.Result);
+        }
+
+        public async void SaveUser(UserModel model)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44345/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            var json = JsonConvert.SerializeObject(model);
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("UserId", model.UserId),
+            });
+            content.Headers.ContentType.MediaType = "application/json";
+            await client.PostAsync("api/values/", content);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
